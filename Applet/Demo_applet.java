69c7983a4de942b357f15411632e47b9afe2f5f6
rootanujraval24 <Anujraval24@gmail.com>
2016-11-05 23:19:38 +0530

    Create Demo_applet.java

---
+++ Applet/Demo_applet.java
@@ -0,0 +1,15 @@
+package applet;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Demo_applet extends Applet{
+
+	public void paint(Graphics g1){
+		g1.drawString("Yo How you doing?", 100, 100);
+		}
+}
+/* 
+<applet code="First.class" width="300" height="300"> 
+</applet> 
+*/  
